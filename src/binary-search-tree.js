69340736584289e7cb93ae55f89d9617acaeb0a0
Valentyn Lysenko <valentynlysenco@gmail.com>
2023-12-21 20:33:38 +0200

    feat: untie the remove from list task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,22 +1,85 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.left = null;
+//     this.right = null;
+//   }
+
+//   add(data) {
+//     if (data < this.data) {
+//       this.addLeft(data);
+//     } else {
+//       this.addRight(data);
+//     }
+//   }
+
+//   addLeft(data) {
+//     if (this.left) {
+//       return this.left.add(data);
+//     }
+//     this.left = new Node(data);
+//   }
+
+//   addRight(data) {
+//     if (this.right) {
+//       return this.right.add(data);
+//     }
+//     this.right = new Node(data);
+//   }
+// }
+
 class BinarySearchTree {
 
+  // constructor() {
+  //   this.root = null;
+  //   return this.root;
+  // }
+  
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.rootElem = null;
+    return this.rootElem;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    // if (!this.rootElem) {
+    //   this.rootElem = new Node(data);
+    //   return this.rootElem;
+    //   // return this.rootElem.add(data);
+    // }
+    // if (data < this.data) {
+    //   this.left = new Node(data);
+    // } else {
+    //   this.right = new Node(data);
+    // }
+    // console.log(data);
+
+        throw new NotImplementedError("Not implemented");
   }
+
+  // constructor addStep(node, data) {
+  //   if (!node) {
+  //     return new Node(data);
+  //   }
+
+  //   if (data < node.data) {
+  //     node.left = addStep(node.left, data);
+  //   }
+
+  //   if (data > node.data) {
+  //     node.right = addStep(node.right, data);
+  //   }
+
+  //   return node;
+  // }
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
